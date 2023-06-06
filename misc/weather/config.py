2a2c54375c08ec9b7941584032877eeb45a08f57
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-06-06 20:33:40 +0300

    class guide, monetary representation, weather app added

---
+++ misc/weather/config.py
@@ -0,0 +1,8 @@
+USE_ROUNDED_COORDS = False
+OPENWEATHER_API = ""  # paste API token here
+OPENWEATHER_URL = (
+    "https://api.openweathermap.org/data/2.5/weather?"
+    "lat={latitude}&lon={longitude}&"
+    "appid=" + OPENWEATHER_API + "&lang=ru&"
+    "units=metric"
+)
